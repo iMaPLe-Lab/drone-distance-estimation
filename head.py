6f553b6809e2f07b8b0e09f88fb9f77227c1039c
Zaid Mayers <zaidmayers@gmail.com>
2025-10-26 22:45:21 -0400

    Turning distance estimation into a classification problem. All executables updated.

--- head.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-def make_regressor(input_dim, hidden=64, dropout=0.2):
-    return nn.Sequential(
-        nn.Linear(input_dim, hidden),
-        nn.ReLU(),
-        nn.Dropout(dropout),
-        nn.Linear(hidden, 1),
-    )
